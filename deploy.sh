25957444bb0c4d6e6826964859946d89b07d51b2
Jason Freeberg <jafreebe@microsoft.com>
2021-10-29 15:24:04 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 # This script is called in the GitHub Actions workflows that deploy the app and supporting files to the ASE.
 
+if [ -z "$1" ]
+then
+    # Append slot to URI
+    SITE_URI="https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/slots/${1}/extensions/onedeploy?api-version=2020-12-01"
+else
+    SITE_URI="https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/extensions/onedeploy?api-version=2020-12-01"
+fi
+
+echo $SITE_URI
+
 # Deploy WAR file
 az storage blob upload      --account-name $STORAGE_ACCOUNT -c $CONTAINER -f ROOT.war
 WAR_URL=$(az storage blob generate-sas --full-uri --permissions r --expiry $EXPIRY --account-name $STORAGE_ACCOUNT -c $CONTAINER -n ROOT.war | xargs)
 echo $WAR_URL
 az rest --method PUT \
         --debug \
-        --uri "https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/extensions/onedeploy?api-version=2020-12-01" \
+        --uri $SITE_URI \
         --body '{ 
             "properties": { 
                 "properties": {
@@ -23,7 +33,7 @@ az rest --method PUT \
 az storage blob upload --account-name $STORAGE_ACCOUNT -c $CONTAINER -f setup/postgresql.jar
 DRIVER_URL=$(az storage blob generate-sas --full-uri --permissions r --expiry $EXPIRY --account-name $STORAGE_ACCOUNT -c $CONTAINER -n postgresql.jar | xargs)
 az rest --method PUT \
-        --uri "https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/extensions/onedeploy?api-version=2020-12-01" \
+        --uri $SITE_URI \
         --body '{ 
             "properties": { 
                 "properties": {
@@ -39,7 +49,7 @@ az rest --method PUT \
 az storage blob upload --account-name $STORAGE_ACCOUNT -c $CONTAINER -f setup/jboss-cli-commands.cli
 SCRIPTS_URL=$(az storage blob generate-sas --full-uri --permissions r --expiry $EXPIRY --account-name $STORAGE_ACCOUNT -c $CONTAINER -n jboss-cli-commands.cli | xargs)
 az rest --method PUT \
-        --uri "https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/extensions/onedeploy?api-version=2020-12-01" \
+        --uri $SITE_URI \
         --body '{ 
             "properties": { 
                 "properties": {
@@ -55,7 +65,7 @@ az rest --method PUT \
 az storage blob upload --account-name $STORAGE_ACCOUNT -c $CONTAINER -f setup/startup.sh
 STARTUP_URL=$(az storage blob generate-sas --full-uri --permissions r --expiry $EXPIRY --account-name $STORAGE_ACCOUNT -c $CONTAINER -n startup.sh | xargs)
 az rest --method PUT \
-        --uri "https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/extensions/onedeploy?api-version=2020-12-01" \
+        --uri $SITE_URI \
         --body '{ 
             "properties": { 
                 "properties": {
@@ -63,4 +73,4 @@ az rest --method PUT \
                 }, 
                 "type": "startup", 
                 }
-            }'
+        }'
