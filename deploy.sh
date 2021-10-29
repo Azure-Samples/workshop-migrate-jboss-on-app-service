e401c52d14501f732756eacf8acaaee73d8ff8e0
Jason Freeberg <jafreebe@microsoft.com>
2021-10-29 13:38:53 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # Deploy WAR file
 az storage blob upload      --account-name $STORAGE_ACCOUNT -c $CONTAINER -f ROOT.war
 WAR_URL=$(az storage blob generate-sas --full-uri --permissions r --expiry $EXPIRY --account-name $STORAGE_ACCOUNT -c $CONTAINER -n ROOT.war | xargs)
+echo $WAR_URL
 az rest --method PUT \
         --debug \
         --uri "https://management.azure.com/subscriptions/${SUBSCRIPTION}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Web/sites/${WEBAPP}/extensions/onedeploy?api-version=2020-12-01" \
